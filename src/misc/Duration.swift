adefa5aa0486052a290825a07f9104421c33eb1a
Benj <benjamin.godard@arcan.fr>
2017-01-04 10:44:38 +0100

    update copyright year

--- src/misc/Duration.swift
+++ src/misc/Duration.swift
@@ -1,5 +1,5 @@
 // Duration.swift
-// Copyright (c) 2016 Nyx0uf
+// Copyright (c) 2017 Nyx0uf
 //
 // Permission is hereby granted, free of charge, to any person obtaining a copy
 // of this software and associated documentation files (the "Software"), to deal
